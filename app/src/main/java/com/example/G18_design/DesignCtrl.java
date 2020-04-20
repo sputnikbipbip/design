7192bebd20bcf1d85ddc6b42a2cd764b69c9e3a9
Daniel Azevedo <azevedo_daniel1994@hotmail.com>
2020-04-20 21:28:55 +0100

    Listeners Ready

--- app/src/main/java/com/example/G18_design/DesignCtrl.java
+++ app/src/main/java/com/example/G18_design/DesignCtrl.java
@@ -2,13 +2,15 @@ package com.example.G18_design;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 
 import com.example.G18_design.model.DesignModel;
 import com.example.G18_design.view.DesignView;
 
-public class DesignCtrl extends Activity {
+public class DesignCtrl extends Activity implements View.OnTouchListener{
 
     DesignModel model;
     DesignView view;
@@ -19,11 +21,40 @@ public class DesignCtrl extends Activity {
         setContentView(R.layout.activity_main);
 
         final Button reset = findViewById(R.id.reset);
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         final Button load = findViewById(R.id.load);
+        load.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.invalidate();
+            }
+        });
         final Button save = findViewById(R.id.save);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         final RadioButton line = findViewById(R.id.Line);
         final RadioButton rect = findViewById(R.id.Rect);
         final RadioButton pixel = findViewById(R.id.Pixel);
         final RadioButton circle = findViewById(R.id.Circle);
     }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch(event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                return true;
+        }
+        return true;
+    }
+
 }
--- app/src/main/java/com/example/G18_design/view/DesignView.java
+++ app/src/main/java/com/example/G18_design/view/DesignView.java
@@ -2,12 +2,22 @@ package com.example.G18_design.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
 import android.view.View;
 
 public class DesignView extends View {
 
-    public DesignView(Context context) {
-        super(context);
+    private final Paint brush;
+
+    public DesignView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        brush = new Paint();
+        brush.setColor(Color.BLACK);
+        brush.setStrokeWidth(5);
+        brush.setStyle(Paint.Style.STROKE);
     }
 
 
